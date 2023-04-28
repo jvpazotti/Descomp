ba29df54e21db5428e1a169078f27c9a7d8beb39
jvpazotti <joaovps@al.insper.edu.br>
2023-04-27 22:53:05 -0300

    2 instrucoes adicionadas e decoder modificado

--- assembler/AssemblerASM_BIN_VHDL/AssemblerASM_BIN_VHDL.py
+++ assembler/AssemblerASM_BIN_VHDL/AssemblerASM_BIN_VHDL.py
@@ -72,7 +72,7 @@ Regras:
 
 
 
-inputASM = 'ASM2.txt' #Arquivo de entrada de contém o assembly
+inputASM = 'ASM3.txt' #Arquivo de entrada de contém o assembly
 outputBIN = 'BIN.txt' #Arquivo de saída que contém o binário formatado para VHDL
 outputMIF = 'initROM.mif' #Arquivo de saída que contém o binário formatado para .mif
 
