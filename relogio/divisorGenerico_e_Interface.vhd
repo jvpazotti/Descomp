ff35726beac356f2f36209add20580149a513e23
jvpazotti <joaovps@al.insper.edu.br>
2023-05-05 15:18:16 -0300

    adicionando td

--- relogio/divisorGenerico_e_Interface.vhd
+++ relogio/divisorGenerico_e_Interface.vhd
@@ -6,23 +6,40 @@ entity divisorGenerico_e_Interface is
    port(clk      :   in std_logic;
       habilitaLeitura : in std_logic;
       limpaLeitura : in std_logic;
-      leituraUmSegundo :   out std_logic
+      leituraUmSegundo :   out std_logic;
+		chave_sw : in std_logic
    );
 end entity;
 
 architecture interface of divisorGenerico_e_Interface is
   signal sinalUmSegundo : std_logic;
   signal saidaclk_reg1seg : std_logic;
+  signal saidaclk_regRapido : std_logic;
+  signal saidaMux : std_logic;
 begin
 
 baseTempo: entity work.divisorGenerico
-           generic map (divisor => 25000000/1200)   -- divide por 10.
+           generic map (divisor => 25000000/1)   -- divide por 10.
            port map (clk => clk, saida_clk => saidaclk_reg1seg);
+			  
+baseTempoRapida: entity work.divisorGenerico
+           generic map (divisor => 25000000/1800)   -- divide por 10.
+           port map (clk => clk, saida_clk => saidaclk_regRapido);
+			  
+mux: entity work.muxGenerico2x1_bit
+			port map (
+			entradaA_MUX => saidaclk_reg1seg,
+			entradaB_MUX => saidaclk_regRapido,
+			seletor_MUX => chave_sw,
+			saida_MUX => saidaMux
+			);
 
 registraUmSegundo: entity work.flipflopGenerico
    port map (DIN => '1', DOUT => sinalUmSegundo,
-         ENABLE => '1', CLK => saidaclk_reg1seg,
+         ENABLE => '1', CLK => saidaMux,
          RST => limpaLeitura);
+			
+
 
 -- Faz o tristate de saida:
 leituraUmSegundo <= sinalUmSegundo when habilitaLeitura = '1' else 'Z';
